9915f7e3a5732388879e0fb5434b4df365881214
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-04-21 04:20:48 -0700

    Commiting a new project

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors);
+// Middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+//Routes
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+
+const api = process.env.API_URL;
+
+//Routers
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+//Database
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Database connection is ready...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Server
+app.listen(3000, () => {
+  //console.log(api);
+  console.log('Server is running on http://localhost:3000');
+});
