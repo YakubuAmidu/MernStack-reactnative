29b81bb1b3aafdbb113bb3d73624306989520363
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-04-23 04:21:34 -0700

    Updating changes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 router.get(`/`, async (req, res) => {
   const userList = await User.find();
@@ -10,12 +11,22 @@ router.get(`/`, async (req, res) => {
   res.send(userList);
 });
 
+router.get(`/:id`, async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    res
+      .status(500)
+      .json({ message: 'The user with the given ID was not found' });
+  }
+  res.status(200).send(user);
+});
+
 router.post(`/`, async (req, res) => {
   let user = new User({
     name: req.body.name,
     email: req.body.email,
     color: req.body.color,
-    passwordHash: req.body.passwordHash,
+    passwordHash: bcrypt.hashSync(req.body.passwordHash, 10),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
     apartment: req.body.apartment,
