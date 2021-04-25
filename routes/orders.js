b42aa87a24fa5fd5f61bdc8945d7b3bd6ffd0d48
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-04-25 04:48:12 -0700

    Updating changes

--- routes/orders.js
+++ routes/orders.js
@@ -41,6 +41,21 @@ router.post(`/`, async (req, res) => {
 
   const orderItemsIdsResolved = await orderItemsIds;
 
+  const totalPrices = await Promise.all(
+    orderItemsIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        'product',
+        'price'
+      );
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
+  console.log(totalPrice);
+
   let order = new Order({
     orderItems: orderItemsIdsResolved,
     shippingAddress1: req.body.shippingAddress1,
@@ -50,7 +65,7 @@ router.post(`/`, async (req, res) => {
     country: req.body.country,
     phone: req.body.phone,
     status: req.body.status,
-    totalPrict: req.body.totalPrict,
+    totalPrict: totalPrice,
     user: req.body.user,
   });
   order = await order.save();
