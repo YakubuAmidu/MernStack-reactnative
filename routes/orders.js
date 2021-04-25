7ad0653c95b3ad51aa1c12135285580341fb0d23
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-04-25 02:41:42 -0700

    Updating changes

--- routes/orders.js
+++ routes/orders.js
@@ -14,7 +14,12 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get(`/:id`, async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user', 'name');
+  const order = await Order.findById(req.params.id)
+    .populate('user', 'name')
+    .populate({
+      path: 'orderItems',
+      populate: { path: 'product', populate: 'category' },
+    });
 
   if (!order) {
     return res.status(500).json({ success: false });
