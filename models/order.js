199a80720159560f699e1634d6f047f6b6d2e04c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-04-25 00:12:05 -0700

    Creating order-item

--- models/order.js
+++ models/order.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true,
-  },
+  orderItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'oderItem',
+      required: true,
+    },
+  ],
 });
 
 exports.Order = mongoose.model('Order', orderSchema);
