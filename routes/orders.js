f4cb9eb67e8a6d25363f409e98f2593ab2165acf
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-04-25 04:25:42 -0700

    Updating changes

--- routes/orders.js
+++ routes/orders.js
@@ -60,4 +60,39 @@ router.post(`/`, async (req, res) => {
   res.send(order);
 });
 
+router.put(`/:id`, async (req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    {
+      new: true,
+    }
+  );
+  if (!order) return res.status(400).send('The order cannot be created');
+  res.send(order);
+});
+
+router.delete(`/:id`, (req, res) => {
+  Order.findByIdAndRemove(req.params.id)
+    .then(async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+        return res
+          .status(200)
+          .json({ success: true, message: 'Ther order is deleted' });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Order not found' });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ success: false, error: err });
+    });
+});
+
 module.exports = router;
