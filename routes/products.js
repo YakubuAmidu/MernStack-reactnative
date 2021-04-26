3fb1ad023312709b08bc203ac1b984adae17bf30
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-04-26 00:05:57 -0700

    Updating changes

--- routes/products.js
+++ routes/products.js
@@ -5,17 +5,17 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
 
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'public/uploads');
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
+    const fileName = file.originalname.split(' ').join('-');
+    cb(null, fileName + '-' + Date.now());
   },
 });
 
-var upload = multer({ storage: storage });
+const uploadOptions = multer({ storage: storage });
 
 router.get(`/`, async (req, res) => {
   // localhost:3000/api/v1/products?categories=2342342,234234
@@ -41,15 +41,17 @@ router.get(`/:id`, async (req, res) => {
   res.send(product);
 });
 
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'), async (req, res) => {
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send('Invalid Category');
+  const fileName = req.file.filename;
+  const basePath = `${req.protocol}://${req.get('host')}//public/upload/`;
 
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
-    image: req.body.image,
+    image: `${basePath}${fileName}`, //"http://localhost:3000/public/upload/image-232323"
     brand: req.body.brand,
     price: req.body.price,
     category: req.body.category,
